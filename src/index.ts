854927b342c2457ee9cdf82207d8e5e42815122c
Margus Liinev <margus.liinev@gmail.com>
2024-05-28 20:06:24 +0300

    add auth middleware and user_id to hono context

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import { createUser, getUserByEmail, getUserByUsername } from './models/user';
-import { createSession } from './models/session';
+import { createUser, getUserByEmail, getUserByEmailWithPassword, getUserByUsername, getUserById } from './models/user';
+import { createSession, getUserBySessionId } from './models/session';
 import { zValidator } from '@hono/zod-validator';
-import { setSignedCookie } from 'hono/cookie';
+import { deleteCookie, getSignedCookie, setSignedCookie } from 'hono/cookie';
 import { usersTable } from './db/schema';
 import { logger } from 'hono/logger';
 import { Hono } from 'hono';
 import { db } from './db';
 import { z } from 'zod';
+
+declare module 'hono' {
+    interface ContextVariableMap {
+        user: number;
+    }
+}
 
 if (!process.env.PORT) throw new Error('PORT is not defined');
 
@@ -25,9 +31,36 @@ const registerSchema = z.object({
         .regex(/.*[A-Za-z].*/, { message: 'Password must contain at least one letter' })
 });
 
+const loginSchema = z.object({
+    email: z.string({ invalid_type_error: 'Email is invalid' }).min(1, { message: 'Email is required' }).email({ message: 'Email is invalid' }),
+    password: z.string({ invalid_type_error: 'Password is invalid' }).min(1, { message: 'Password is required' })
+});
+
 const app = new Hono();
 
 app.use(logger());
+
+app.notFound(async (c) => {
+    return c.json({ success: false, message: 'Not Found' }, 404);
+});
+
+app.onError(async (err, c) => {
+    return c.json({ success: false, message: err.message }, 500);
+});
+
+app.use('/users/*', async (c, next) => {
+    const session = await getSignedCookie(c, process.env.SESSION_SECRET!, '__session');
+    if (!session) return c.json({ success: false, message: 'Unauthorized' }, 401);
+
+    const user = await getUserBySessionId(Number(session));
+    if (!user) {
+        deleteCookie(c, '__session', { path: '/' });
+        return c.json({ success: false, message: 'Unauthorized' }, 401);
+    }
+
+    c.set('user', user.id);
+    await next();
+});
 
 app.get('/', async (c) => {
     return c.json({ success: true, message: 'Hello World' });
@@ -38,7 +71,12 @@ app.get('/users', async (c) => {
     return c.json({ success: true, data: users });
 });
 
-app.post('/users', zValidator('json', registerSchema), async (c) => {
+app.get('/users/me', async (c) => {
+    const user = await getUserById(c.get('user'));
+    return c.json({ success: true, data: user });
+});
+
+app.post('/auth/register', zValidator('json', registerSchema), async (c) => {
     const body = c.req.valid('json');
 
     const existingUsername = await getUserByUsername(body.username);
@@ -56,10 +94,7 @@ app.post('/users', zValidator('json', registerSchema), async (c) => {
     const session = await createSession(newUser.id);
     if (!session) return c.json({ success: false, message: 'Failed to create session' }, 500);
 
-    const hasher = new Bun.CryptoHasher('sha256');
-    const encryptedSession = hasher.update(String(session.id)).digest('hex');
-
-    await setSignedCookie(c, '__session', encryptedSession, process.env.SESSION_SECRET!, {
+    await setSignedCookie(c, '__session', String(session.id), process.env.SESSION_SECRET!, {
         path: '/',
         httpOnly: true,
         secure: Bun.env.NODE_ENV === 'production',
@@ -67,7 +102,30 @@ app.post('/users', zValidator('json', registerSchema), async (c) => {
         sameSite: 'lax'
     });
 
-    return c.json({ success: true, message: 'User created' });
+    return c.json({ success: true, message: 'User register successful' });
+});
+
+app.post('/auth/login', zValidator('json', loginSchema), async (c) => {
+    const body = c.req.valid('json');
+
+    const user = await getUserByEmailWithPassword(body.email);
+    if (!user) return c.json({ success: false, message: 'Invalid email or password' }, 400);
+
+    const validPassword = await Bun.password.verify(body.password, user.password);
+    if (!validPassword) return c.json({ success: false, message: 'Invalid email or password' }, 400);
+
+    const session = await createSession(user.id);
+    if (!session) return c.json({ success: false, message: 'Failed to create session' }, 500);
+
+    await setSignedCookie(c, '__session', String(session.id), process.env.SESSION_SECRET!, {
+        path: '/',
+        httpOnly: true,
+        secure: Bun.env.NODE_ENV === 'production',
+        maxAge: 7 * 24 * 60 * 60,
+        sameSite: 'lax'
+    });
+
+    return c.json({ success: true, message: 'User login successful' });
 });
 
 export default { fetch: app.fetch, port: process.env.PORT };
