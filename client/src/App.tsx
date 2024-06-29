de67783c1619c59656ba4732f3e8bf7dfdc74beb
Margus Liinev <margus.liinev@gmail.com>
2024-06-29 21:03:29 +0300

    update packages, add bit of styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,31 @@
 import { useEffect, useState } from 'react';
-import { APIRoutes } from '../../src/server';
+import { APIRoutes } from '../../src/index';
 import { hc } from 'hono/client';
 
 const client = hc<APIRoutes>('/');
 
 function App() {
+    const [loading, setLoading] = useState<boolean>(false);
     const [message, setMessage] = useState<string>('');
 
     useEffect(() => {
         const fetchData = async () => {
+            setLoading(true);
+            await new Promise((resolve) => setTimeout(resolve, 1000));
             const res = await client.api.health.ok.$get();
             const data = await res.json();
             setMessage(data.message);
+            setLoading(false);
         };
 
         fetchData();
     }, []);
 
-    return <main>{message}</main>;
+    return (
+        <main style={{ backgroundColor: '#222', height: '100vh', width: '100vw', margin: '0px', display: 'grid', justifyContent: 'center', alignItems: 'center' }}>
+            <h1 style={{ margin: '0px', color: 'white' }}>{loading ? 'Loading...' : message}</h1>
+        </main>
+    );
 }
 
 export default App;
